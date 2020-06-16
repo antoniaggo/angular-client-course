e1bd09b1e69579ce0de6c774602bb9b7aff7dd8e
Antonia Gallardo <antonia.gallardo.gomez@gmail.com>
2020-06-16 19:23:22 +0200

    Component Inputs y el decorador @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-client-course';
+  
+  course1: Course = COURSES[0];
+
+  course2: Course = COURSES[1];
+
+  course3: Course = COURSES[3];
+
+
 }
