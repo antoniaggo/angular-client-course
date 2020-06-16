0b9aed76b33df7006a1878223d7b5ad387342953
Antonia Gallardo <antonia.gallardo.gomez@gmail.com>
2020-06-16 20:40:13 +0200

    Directiva *ngFor de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,7 @@ import { Course } from './model/course';
 })
 export class AppComponent {
   
-  course1: Course = COURSES[0];
-
-  course2: Course = COURSES[1];
-
-  course3: Course = COURSES[3];
+  courses: Course[] = COURSES;
 
 
 }
